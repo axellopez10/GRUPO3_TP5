eec0689117859a4dd4d18c0171ab27e92d67dc2c
Kamil Jofré <kamiljelias@gmail.com>
2025-09-16 20:25:43 -0300

    Kamil Intento durante clase

--- src/grupo3_tp5/Contacto.java
+++ src/grupo3_tp5/Contacto.java
@@ -10,5 +10,59 @@ package grupo3_tp5;
  * @author Adrian
  */
 public class Contacto {
+    private double DNI;
+    private String nombre;
+    private String apellido;
+    private String Ciudad ;
+    private String dirección;
+
+    public Contacto(double DNI, String nombre, String apellido, String Ciudad, String dirección) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Ciudad = Ciudad;
+        this.dirección = dirección;
+    }
+
+    public double getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(double DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String Ciudad) {
+        this.Ciudad = Ciudad;
+    }
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+    
     
 }
---
+++ src/grupo3_tp5/DirectorioTelefonico.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo3_tp5;
+
+import java.util.*;
+import sun.security.rsa.RSAUtil.KeyType;
+
+/**
+ *
+ * @author kamil
+ */
+public class DirectorioTelefonico {
+    private TreeMap<Long, Contacto> Directorio = new TreeMap<>();
+    
+    
+    public DirectorioTelefonico() {
+        this.Directorio = new TreeMap<>();
+    }
+    
+    
+    public void agregarContacto(Long telefono, Contacto contacto) {
+        if (!Directorio.containsKey(telefono)) {
+            Directorio.put(telefono, contacto);
+            System.out.println("Contacto agregado: " + contacto);
+        } else {
+            System.out.println("El teléfono ya existe en el directorio.");
+        }
+    }
+    
+    
+    public void buscarContacto(Long telefono){
+        Directorio.get(telefono);
+    }
+    
+    public Set<Long> buscarTelefono(String apellido) {
+        Set<Long> telefonos = new TreeSet<>();
+        for (Map.Entry<Long, Contacto> entry : Directorio.entrySet()) {
+            if (entry.getValue().getApellido().equalsIgnoreCase(apellido)) {
+                telefonos.add(entry.getKey());
+            }
+        }
+        return telefonos;
+    }
+    
+    public ArrayList<Contacto> buscarContactos(String ciudad) {
+       ArrayList<Contacto> lista = new ArrayList<>();
+       for(Contacto c : Directorio.values()) {
+           if(c.getCiudad().equalsIgnoreCase(ciudad)){
+               lista.add(c);
+           }
+       }
+       return lista;
+    }
+    
+    public void borrarContacto(Long telefono){
+        if(Directorio.containsValue(telefono)){
+            Contacto eliminado = Directorio.remove(telefono);
+            System.out.println("Contacto eliminado: "+eliminado);
+        } else{
+            System.out.println("No se encontro este telefono.");
+        }
+    }
+    
+    //debug de onda
+    public void mostrarTodos(){
+        for(Map.Entry<Long, Contacto> entry : Directorio.entrySet()){
+            System.out.println(entry.getKey()+" -> "+entry.getValue());
+        }
+    }
+}
